08ea36d0bb18a597b003202b5bd2fff5c6dba91f
Wanying Zhu <wanying.zhu.1@vanderbilt.edu>
2024-01-10 14:21:04 -0600

    Parse parguments

--- 20231211_rerun/code/ML_08_boosting_model_train.py
+++ 20231211_rerun/code/ML_08_boosting_model_train.py
@@ -5,10 +5,26 @@
 # 2. Gradient boost
 # 3. XGBoost
 # Options of base estimator: Linear regression Multilayer perceptron (MLP), Decision tree regressor
-import argparse
-
 # Refer to code ML_03_model_train.py and ML_07_boosting_models_test_run.ipynb
-
+'''
+Example call
+lip_type=species
+lipid="PC(44:5)"
+python ML_08_boosting_model_train.py \
+--output_prefix test_run \
+--output_dir /data100t1/home/wanying/CCHC/lipidomics/20231211_rerun/outputs/prediction_models/AdaBoost/${lip_type} \
+--dosage_dir /data100t1/home/wanying/CCHC/lipidomics/20231211_rerun/inputs/genotype_dosage/train/lipid_${lip_type} \
+--dosage_fn lipid_${lip_type}_chr*.pval_0.001_maf_0.05.vcf.dosage.gz \
+--gwas_snp_dir /data100t1/home/wanying/CCHC/lipidomics/20231211_rerun/outputs/fastGWA/lipid_${lip_type}_filter_by_pval_1e-07 \
+--gwas_snp_fn PC-44:5-_SNPs.pval_1e-07.txt \
+--lip_name ${lipid} \
+--trait_fn /data100t1/home/wanying/CCHC/lipidomics/20231211_rerun/inputs/lipid_trait/lipid_${lip_type}_ID_matched.no_dup.residual.train.txt \
+--multiallelic False \
+--train True \
+--n_estimator 100 \
+--boost_type Ada
+'''
+import argparse
 from sklearn.ensemble import AdaBoostRegressor
 from sklearn.ensemble import GradientBoostingRegressor
 import logging
@@ -29,8 +45,12 @@ def config_logging(log_fn):
     logging.root.handlers = [] # Remove potential handler set up by others
     # logging.info to console and save to log file. Change mode='a' for appending mode
     logging.basicConfig(level=logging.INFO,
-                        handlers=[logging.FileHandler(filename=log_fn, mode='w'), logging.StreamHandler()],
+                        handlers=[logging.FileHandler(filename=log_fn, mode='a'), logging.StreamHandler()],
                         format='# %(name)s - %(levelname)s - %(message)s')
+    # Start logging
+    logging.info(__file__)
+    logging.info('# ' + '#' * 20 + ' Run started on' + datetime.datetime.now().strftime('%Y-%m-%d') + ' ' +'#' * 20)
+    logging.info('')
 
 def parse_arguments():
     '''
@@ -53,28 +73,173 @@ def parse_arguments():
                         help='Name of the lipid to be processed')
     parser.add_argument('--trait_fn', type=str, help='File name of lipidomics data (or other trait). File must in sample x trait format. Must have a column matches genotype IDs',
                         default='/data100t1/home/wanying/CCHC/lipidomics/20231211_rerun/inputs/lipid_trait/lipid_species_ID_matched.no_dup.residual.train.txt')
-    parser.add_argument('--n_alphas', type=int, default=100,
-                        help='Define how many alphas to test in CV. Default is 10. JTI used 100 as defined in R glmnet()')
+    parser.add_argument('--n_estimator', type=int, default=100,
+                        help='Define how many estimator to use in the model')
     parser.add_argument('--multiallelic', type=str, default='False',
                         help='If false, multiallelic SNPs will be removed from model fitting')
     parser.add_argument('--train', type=str, default='True',
                         help='If true, will not fill with NaN if a SNP is not found. Missing values will cause errors')
-    parser.add_argument('--reg_type', type=str, default='elastic_net', choices=['elastic_net', 'ridge', 'lasso', 'ols'],
-                        help="Type of regression. Choose from: 'elastic_net', 'ridge', 'lasso' and 'ols'")
-    args = parser.parse_args()
+    parser.add_argument('--boost_type', type=str, default='elastic_net', choices=['Ada', 'Gradient', 'XG'],
+                        help='Type of boosting. Choose from AdaBoost, gradient and XG')
+    return parser.parse_args()
 
-    print('\n# Arguments used:')
+def sanity_checks():
+    '''
+    Check arguments
+    :return:
+    '''
+    args.output_prefix = f"{args.output_prefix}.{datetime.datetime.now().strftime('%Y%m%d_%H:%M:%S')}"
+
+    # Make all directory to be absolute path
+    args.output_dir = os.path.expanduser(args.output_dir)
+    args.dosage_dir = os.path.expanduser(args.dosage_dir)
+    args.gwas_snp_dir = os.path.expanduser(args.gwas_snp_dir)
+    args.trait_fn = os.path.expanduser(args.trait_fn)
+
+    msg = '# Check output directory:' # For logging purpose
+    if not os.path.isdir(args.output_dir):
+        msg += ' Output directory does not exist. Create one at: ' + args.output_dir
+        os.makedirs(args.output_dir) # Use makedirs to create folds recursively
+    else:
+        msg += ' PASS'
+
+    # Start logging
+    log_fn = os.path.join(args.output_dir, args.output_prefix+'.log')
+    config_logging(log_fn)
+    logging.info('# ' + '*' * 20 + ' Sanity checks ' + '*' * 20)
+    logging.info(msg)
+
+    # Check if files exist
+    logging.info('# Check dosage files')
+    all_dosage_files_exist = True
+    for i in range(1, 23):
+        dosage_fn = os.path.join(args.dosage_dir, args.dosage_fn.replace('*', str(i)))
+        if not os.path.isfile(dosage_fn):
+            logging.error('# - CHR%s dosage file not found: %s' % (i, dosage_fn))
+            all_dosage_files_exist = False
+        else:
+            logging.info('# - CHR%s: PASS' % i)
+    if not all_dosage_files_exist:
+        logging.error('# - Missing dosage files. Exit')
+        exit()
+
+    logging.info('# Check (filtered) GWAS SNP file: ')
+    if not os.path.isfile(os.path.join(args.gwas_snp_dir, args.gwas_snp_fn)):
+        logging.error('# - filtered GWAS result not found: ' + os.path.join(args.gwas_snp_dir, args.gwas_snp_fn))
+        exit()
+    else:
+        logging.info('# - PASS')
+
+    logging.info('# Check trait file (residuals of lipidomic measures):')
+    if not os.path.isfile(args.trait_fn):
+        logging.error('# - trait file not found: ' + args.trait_fn)
+        exit()
+    else:
+        logging.info('# - PASS')
+
+    if args.multiallelic.upper()[0] == 'F' or args.multiallelic == '0':
+        args.multiallelic = False
+    else:  # Do not drop multiallelic sites if True
+        args.multiallelic = True
+
+    if args.train.upper()[0] == 'F' or args.train == '0':
+        args.train = False
+    else:  # Do not fill missing values with NA # Seems unnecessary?
+        args.train = True
+    logging.info('# Arguments used:')
     for arg in vars(args):
-        print(f'# - {arg}:', getattr(args, arg))
-    return parser
+        logging.info('# - %s: %s' % (arg, getattr(args, arg)))
+    return msg
 
 
+def load_dosage(snp_dir, snp_fn, dosage_dir, dosage_fn):
+    logging.info('# - Load filtered SNPs')
+    df_snps = pd.read_csv(os.path.join(snp_dir, snp_fn), sep='\t')
+
+    logging.info('# - Get SNP dosage from CHR1-22')
+    snps_all, dosage_all, lst_sample_ids = [], [], [] # lists of SNPs and dosage found and used in model, sample IDs
+    total_num_snps = len(df_snps)
+    c_all_loaded = 0 # track number of all loaded snps
+    for chr_num, df in df_snps.groupby(by='CHR'):
+        logging.info('# - Get dosage from CHR%s'%chr_num)
+        # Create a list of positions to search by tabix
+        lst_pos = 'chr' + df['CHR'].astype('str') + ':' + df['POS'].astype('str') + '-' + df['POS'].astype('str')
+        total, c = len(df), 0 # Number of SNPs to be found and actually loaded  on current chromosome
+        count_missing = 0  # Count number of missing or skipped sites
+        for pos in lst_pos:
+            cmd = f'tabix {os.path.join(dosage_dir, dosage_fn.replace("*", str(chr_num)))} {pos} | cut -f 1-5,10-'
+            dosage = subprocess.run(cmd, shell=True, text=True, capture_output=True).stdout.strip()
+            if dosage == '':
+                count_missing += 1
+                continue # If the position is not found, tabix call returns empty string
+            if not args.multiallelic: # Ignore multiallelic sites when args.multiallelic is False
+                if len(dosage.split('\n')) > 1:
+                    count_missing += 1
+                    continue
+            tmp_lst = dosage.split('\n')[0].split()
+            snps_all.append(tmp_lst[2])
+            dosage_all.append(tmp_lst[5:])
+            c += 1
+            if c == 1: # Get sample IDs from header by using -H flag with tabix
+                cmd = f'tabix -H {os.path.join(dosage_dir, dosage_fn.replace("*", str(chr_num)))} {pos} | cut -f 10-'
+                sample_ids = subprocess.run(cmd, shell=True, text=True, capture_output=True).stdout
+                lst_sample_ids = sample_ids.split('\n')[-2].split('\t')
+        logging.info('#    - CHR%s: SNPs loaded %s/%s; Multiallelic or missing SNPs: %s' % (chr_num, c, total, count_missing))
+        c_all_loaded += c
+    df_dosage_all = pd.DataFrame(data=np.array(dosage_all), columns=lst_sample_ids, index=snps_all)
+    logging.info('# - Total number of SNPs loaded: %s' % c)
+    logging.info('')
+    return df_dosage_all.T.reset_index().rename(columns={'index':'genotype_ID'})
+
+
+# TODO
+# fix below code
+def load_trait_values(trait_dir, trait_fn):
+    # create a dictionary for modified lipid name matching
+    fn_trait_name_matching = f'/data100t1/home/wanying/CCHC/lipidomics/20231211_rerun/inputs/lipid_trait/lipid_{lipid_type}.list'
+    df_trait_name_matching = pd.read_csv(fn_trait_name_matching, sep='\t', header=None).rename(columns={0:'Lipid'})
+    df_trait_name_matching['Lipid_name'] = df_trait_name_matching['Lipid'].apply(lambda x: x.replace('\\', '-').replace('/', '-').replace('(','-').replace(')','-').replace(' ', '_'))
+    dict_trait_name_matching = df_trait_name_matching.set_index(keys='Lipid_name').to_dict()['Lipid']
+
+    logging.info('# Load trait values: %s'%lipid)
+    # trait_dir = '/data100t1/home/wanying/CCHC/lipidomics/20231211_rerun/inputs/lipid_trait'
+    # trait_fn = f'lipid_{lipid_type}_ID_matched.no_dup.residual.{data_type}.txt'
+    df_trait = pd.read_csv(os.path.join(trait_dir, trait_fn), sep='\t')
+    return df_trait, dict_trait_name_matching
+
+# def load_trait(trait_fn, genotype_ids):
+#     '''
+#     Load dependent variable Y (ie. residuals of lipidomics)
+#     :param trait_fn: file name (of lipidomics data or other trait).
+#                      File must in sample x trait format.
+#                      Must have a column matches genotype IDs
+#            genotype_ids: array-like genotype IDs for re-ordering
+#     :return: a dataframe of traits to be used for model training
+#     '''
+#     if trait_fn.endswith('.csv'):
+#         df = pd.read_csv(trait_fn)
+#     else:
+#         df = pd.read_csv(trait_fn, sep='\t')
+#
+#     # Re-order lipidomics data so that sample IDs match the order in genotype file
+#     # df.set_index(keys='genotyoe_IDs', inplace=True)
+#     return df.set_index(keys='genotype_ID').reindex(index=genotype_ids).reset_index()
 
 # #################### End of helper functions ####################
 
-# Store some logging messages
-msg = '#' * 80
-msg += '\n# ' + '#'*20 + datetime.datetime.now().strftime('%Y-%m-%d') + '#'*20
-msg += '\n#' * 80
-
 args = parse_arguments()
+sanity_checks()
+
+logging.info('# ' + '*' * 20 + ' Get dosage ' + '*' * 20)
+# Load dosage
+df_dosage = load_dosage(snp_dir=args.gwas_snp_dir,
+                     snp_fn=args.gwas_snp_fn,
+                     dosage_dir=args.dosage_dir,
+                     dosage_fn=args.dosage_fn)
+
+# Load trait
+logging.info('# ' + '*' * 20 + ' Get trait values ' + '*' * 20)
+df_trait = ()
+
+# Train model
+
